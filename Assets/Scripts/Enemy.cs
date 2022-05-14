5831e0a422d5622e4f82c2bfda7ac2f024556973
Thogan24 <thogan1924@gmail.com>
2022-05-14 18:27:38 -0400

    Enemy follows player + Animation
     - Enemy now follows player and has a set view distance
     - Added Transition from home screen to game (doesn't work)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour{
 
-    public GameObject player;
+    public GameObject player; 
 
+    // Attacking Variables
     public int bossHealth = 100;
     public int damageAmount;
     public int damageCooldown;
     //public Collider collider;
 
+    // AI Variables
+    public float lookRadius = 10f;
+    public NavMeshAgent agent;
+    Transform target;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
@@ -21,10 +30,21 @@ public class Enemy : MonoBehaviour{
         // look at player
         transform.LookAt(player.transform);
 
-
         //follow the player
+        float distance = Vector3.Distance(target.position, transform.position);
 
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
 
+            if (distance <= agent.stoppingDistance)
+            {
+                //Attack the target
+                //Look at the target potentially needed
+            }
+        }
+
+        //If the player hits the boss, take damage
         if (player.transform.GetChild(3).GetComponent<Collider>().bounds.Intersects(GetComponent<Collider>().bounds))
         {
             Debug.Log(bossHealth);
@@ -45,5 +65,11 @@ public class Enemy : MonoBehaviour{
     void Attack()
     {
 
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
